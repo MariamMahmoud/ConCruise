c90390e8811a75184ae30c5474f02ab6133352d0
Mariam Mahmoud <mariam.mahmoud.saad#gmail.com>
2020-07-27 20:49:38 +0200

    feat:  add calc score service

---
+++ src/Services/calculateScore.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const geolib = require('geolib');
+
+module.exports = (rider, cruiser) => {
+    let score = 0;
+    let distance = getDistance(
+        rider.current_location,
+        cruiser.current_location
+    );
+
+    distance /= 1000;
+
+    if(distance <= 3) {
+        score += 7;
+    } else if(distance <= 5 ) {
+        score += 3;
+    }
+
+    if(rider.rating >= cruiser.rating) score = score + 2
+
+    if(rider.number_of_rides <= 2 && cruiser.number_of_rides >= 3 ) {
+        score += 5;
+    } else if(rider.number_of_rides > 2 && cruiser.number_of_rides < 3) {
+        score += 2;
+    }
+
+    return score;
+};
